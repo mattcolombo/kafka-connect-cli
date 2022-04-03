9e2ab2d073681cde0681c5ab59d8da49d347e6aa
Mattia Colombo @ GRAM <mattomattone89@outlook.com>
2022-04-03 13:11:09 +0100

    added simulation/validation capability to connector create operation (including refactoring of some functions)

--- cmd/connector/connector.go
+++ cmd/connector/connector.go
@@ -1,6 +1,13 @@
 package connector
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/mattcolombo/kafka-connect-cli/utilities"
 	"github.com/spf13/cobra"
 )
 
@@ -19,11 +26,62 @@ var ConnectorCmd = &cobra.Command{
 }
 
 func init() {
-	ConnectorCmd.AddCommand(ConnectorListCmd)
-	ConnectorCmd.AddCommand(ConnectorGetCmd)
 	ConnectorCmd.AddCommand(ConnectorCreateCmd)
 	ConnectorCmd.AddCommand(ConnectorUpdateCmd)
+	ConnectorCmd.AddCommand(ConnectorListCmd)
+	ConnectorCmd.AddCommand(ConnectorGetCmd)
 	ConnectorCmd.AddCommand(ConnectorDeleteCmd)
 	ConnectorCmd.AddCommand(ConnectorPauseCmd)
 	ConnectorCmd.AddCommand(ConnectorResumeCmd)
+	//ConnectorCmd.AddCommand(ConnectorRestartCmd)
+}
+
+/*
+Common functions around extracting and building the request bodies
+
+These functions are not added to the utilities since they are only used within
+this package so do not need to be available to the rest of the CLI.
+They may be moved in the future if required while refactoring.
+*/
+func extractRequestBody(filePath string) []byte {
+	fmt.Println("I am importing the configuration file from", filePath) // control statement print - TOREMOVE
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println("Connector config file not found!")
+			os.Exit(1)
+		} else {
+			fmt.Println("Error while opening the connector configuration file")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+	return file
+}
+
+func extractConnectorConfig(file []byte) []byte {
+	/*
+		Update endpoint expects to receive only the configuration, not the full JSON structure of the configuration file.
+		In order to build that (since we don't know the specific structure of the connector being updated) we first unmarshal it into
+		a structure and extract the "config" structure. This is extracted as a map, and then this map is Marshaled again as JSON to be
+		able to pass it to the PUT Rest endpoint
+
+		NOTE: the structure is shared by different commands so is defined in connector.go in the common variables section
+	*/
+	// unmarshaling the config file and extracting the "config" part into a map --> Config map[string]string `json:"config"`
+	var jsonConfig connectConfig
+	err := json.Unmarshal(file, &jsonConfig)
+	if err != nil {
+		fmt.Printf("Error while extracting the connector configuration %s\n", err)
+		os.Exit(1)
+	}
+
+	// marshaling the map obtained in the previous step to build the request body
+	jsonData, err := json.Marshal(jsonConfig.Config)
+	if err != nil {
+		fmt.Printf("Error while building the request body %s\n", err)
+		os.Exit(1)
+	}
+	utilities.PrettyPrintJson(jsonData) //TOREMOVE control statement
+	return jsonData
 }
--- cmd/connector/create.go
+++ cmd/connector/create.go
@@ -2,25 +2,30 @@ package connector
 
 import (
 	"bytes"
-	"errors"
+	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
-	"os"
+	"strings"
 
 	"github.com/mattcolombo/kafka-connect-cli/utilities"
 	"github.com/spf13/cobra"
 )
+
+var validate bool
 
 var ConnectorCreateCmd = &cobra.Command{
 	Use:   "create",
 	Short: "short description",
 	Long:  "long description",
 	Run: func(cmd *cobra.Command, args []string) {
-		var path string = "/connectors"
-		fmt.Println("making a call to", path) // control statement print - TOREMOVE
-		response, err := utilities.DoCallByPath(http.MethodPost, path, buildCreateRequestBody())
+		var response *http.Response
+		var err error
+		connectorConfiguration := extractRequestBody(connectorPath)
+		if validate {
+			response, err = doValidateCall(connectorConfiguration)
+		} else {
+			response, err = doCreateCall(connectorConfiguration)
+		}
 		if err != nil {
 			fmt.Printf("The HTTP request failed with error %s\n", err)
 		} else {
@@ -32,28 +37,29 @@ var ConnectorCreateCmd = &cobra.Command{
 func init() {
 	ConnectorCreateCmd.Flags().StringVarP(&connectorPath, "config-path", "", "", "path to the connector configuration file (required)")
 	ConnectorCreateCmd.MarkFlagRequired("config-path")
+	ConnectorCreateCmd.Flags().BoolVarP(&validate, "validate", "", false, "validates the connector configuration; the creation is only simulated and is NOT executed")
 }
 
-func buildCreateRequestBody() io.Reader {
-	fmt.Println("I am importing the configuration file from", connectorPath) // control statement print - TOREMOVE
-	file, err := ioutil.ReadFile(connectorPath)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			fmt.Println("Connector config file not found!")
-			os.Exit(1)
-		} else {
-			fmt.Println("Error while opening the connector configuration file")
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	}
-
-	return bytes.NewBuffer(file)
+func doCreateCall(configFile []byte) (*http.Response, error) {
+	var path string = "/connectors"
+	fmt.Println("making a call to", path) // control statement print - TOREMOVE
+	requestBody := bytes.NewBuffer(configFile)
+	return utilities.DoCallByPath(http.MethodPost, path, requestBody)
 }
 
-/*
+func doValidateCall(configFile []byte) (*http.Response, error) {
+	pluginName := extractPluginType(configFile)
+	var path string = "/connector-plugins/" + pluginName + "/config/validate"
+	fmt.Println("making a call to", path) // control statement print - TOREMOVE
+	configData := extractConnectorConfig(configFile)
+	requestBody := bytes.NewBuffer(configData)
+	return utilities.DoCallByPath(http.MethodPut, path, requestBody)
+}
+
+func extractPluginType(file []byte) string {
 	var jsonConfig connectConfig
 	json.Unmarshal(file, &jsonConfig)
-
-	fmt.Println(jsonConfig.Config["connector.class"])
-*/
+	pluginClass := jsonConfig.Config["connector.class"]
+	pluginName := strings.Split(pluginClass, ".")
+	return pluginName[len(pluginName)-1]
+}
