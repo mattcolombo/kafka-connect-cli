42be5cc360b5e66e57b0b8e44271bb1852419ade
Mattia Colombo @ GRAM <mattomattone89@outlook.com>
2022-10-15 15:04:53 +0100

    formatted and added Git commit to version command

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -1,18 +1,21 @@
 package version
 
 import (
-	"fmt"
-
+	"github.com/mattcolombo/kafka-connect-cli/utilities"
 	"github.com/spf13/cobra"
 )
 
-var version string = "kconnect-cli : {Major: 0, Minor: 0, Full version: 0.0.1}"
+var version = []byte(`{
+	"Major": "0", 
+	"Minor": "0", 
+	"GitVersion": "---", 
+	"GitCommit": "---"}`)
 
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "shows the CLI version",
 	Long:  "Shows the CLI version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
+		utilities.PrettyPrintJson(version)
 	},
 }
